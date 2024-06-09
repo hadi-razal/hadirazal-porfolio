5c038f9d025b01ef8e6220117e1fe0736eb83016
hadi-razal <hadhirasal22@gmail.com>
2024-06-09 14:15:58 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/public/favicon.png" sizes="any" />
+        <link rel="icon" href="/public/favicon.ico" sizes="any" />
         <link
           rel="apple-touch-icon"
           href="/public/apple-touch-icon.png"
