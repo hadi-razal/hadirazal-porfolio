a46919c6e3f1c6188eb6d38db807331eff7a4ee7
Hadi Razal <hadhirasal22@gmail.com>
2024-06-20 14:31:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { RiInstagramFill, RiTwitterFill } from "react-icons/ri";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-10">
-      <div className="flex items-center justify-center">
-        <h1 className="text-[30px] ">Coming Soon...</h1>
-      </div>
+     // <div className="flex items-center justify-center">
+       // <h1 className="text-[30px] ">Coming Soon...</h1>
+     // </div>
       <div className="flex items-center justify-center gap-6">
 
         <div className="flex items-center justify-center">
