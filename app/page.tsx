808320e6ddab14276fd8444e3090e75215f4fc18
hadi-razal <hadhirasal22@gmail.com>
2024-06-27 03:00:19 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,9 @@ const Home = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
-        className="hero flex flex-col md:flex-row items-center justify-center py-20 px-5 md:px-20"
+        className="hero flex flex-col md:flex-row items-center justify-center py-10 px-5 md:px-20"
       >
-        <div className="image-container mb-10 md:mb-0 md:mr-10">
+        <div className="image-container mb-5 md:mb-0 md:mr-10">
           <Image
             src="https://i.pinimg.com/originals/83/be/23/83be236f0528c6a867715f8c6f9f575f.jpg" // Replace with Hadi's headshot
             alt="Hadi Razal - Full-Stack Developer &amp; Marketing Mastermind"
@@ -53,7 +53,7 @@ const Home = () => {
       </motion.div>
 
       {/* About Section */}
-      <div className="about px-5 md:px-20 py-10">
+      <div className="about px-5 md:px-20 py-5">
         <motion.h3
           initial={{ y: -20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -75,7 +75,7 @@ const Home = () => {
           initial={{ y: -20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, delay: 0.9 }}
-          className="skills mt-10"
+          className="skills mt-5"
         >
           <h4 className="text-2xl md:text-3xl font-semibold text-gray-800">Skills &amp; Expertise</h4>
           <ul className="list-disc ml-5 mt-5 text-gray-600">
@@ -89,7 +89,7 @@ const Home = () => {
       </div>
 
       {/* Contact Section */}
-      <div className="contact px-5 md:px-20 py-10">
+      <div className="contact px-5 md:px-20 py-5">
         <motion.h3
           initial={{ y: -20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -108,20 +108,26 @@ const Home = () => {
         </motion.p>
         <motion.button
           whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.3 }}
-          className="bg-orange-500 text-white py-2 px-4 rounded-md mt-10 hover:bg-orange-700"
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.7 }}
+          className="bg-orange-500 text-white py-2 px-4 rounded-md mt-5 hover:bg-orange-700"
         >
           Message Me
         </motion.button>
-        <div className="social mt-10 flex justify-center md:justify-start w-full">
-          <p className='font-light'>Follow On</p>
+
+        <motion.div initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.7 }} className="social mt-10 flex items-center justify-center md:justify-start w-full opacity-50">
+          <p className='font-light opacity-50'>Follow On</p>
           <Link target="_blank" href="https://twitter.com/Hadi_Razal" >
             <RiTwitterFill className="w-8 h-8 text-gray-600 hover:text-orange-500 ml-5" />
           </Link>
           <Link target="_blank" href="https://www.instagram.com/hadi_razal/" >
             <RiInstagramFill className="w-8 h-8 text-gray-600 hover:text-orange-500 ml-5" />
           </Link>
-        </div>
+        </motion.div>
+
       </div>
     </div>
   );
